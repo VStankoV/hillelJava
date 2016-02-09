c9b2a6d1d26893db0bc308f8602868bd65d7d32f
valeronS <stankov.valeriy@mail.ru>
2016-02-09 21:20:12 +0200

    first lesson; Hello world!

---
+++ src/MyFirstClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Valeron on 09.02.2016.
+ */
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+
+        long msFromBC = 2016L * 365 * 3600 * 1000;
+
+        System.out.println(msFromBC);
+    }
+
+}
