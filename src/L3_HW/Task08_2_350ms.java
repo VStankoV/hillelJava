81fd5a74d0edb084bd826b25621fe536d126ff6d
valeronS <gallardo_666@mail.ru>
2016-02-17 13:06:00 +0200

    add Lesson 3 HomeWork; task 1-8

---
+++ src/L3_HW/Task08_2_350ms.java
@@ -0,0 +1,42 @@
+package L3_HW;
+
+public class Task08_2_350ms {
+	public static void main(String[] args) {
+		//8. Найти все счастливые билеты от 100000 до 999999
+
+		int firstHalfSum, secondHalfSum;
+		int counter = 0;
+
+		for (int num = 100000; num <= 999999; num++) {
+			String s = String.valueOf(num);
+			firstHalfSum = 0;
+			for (int i = 0; i <= 2; i++) {
+				firstHalfSum += Character.getNumericValue(s.charAt(i));
+			}
+			secondHalfSum = 0;
+			for (int i = 3; i <= 5; i++) {
+				secondHalfSum += Character.getNumericValue(s.charAt(i));
+			}
+			if (firstHalfSum == secondHalfSum) {
+				System.out.println(num);
+				counter++;
+			}
+
+
+		}
+		System.out.println("Всего счастливых билетов : " + counter);
+
+	}
+}
+/* OUTPUT:
+100 001
+100 010
+100 100
+101 002...
+
+998 899
+998 989
+998 998
+999 999
+Всего счастливых билетов : 50412
+*/
