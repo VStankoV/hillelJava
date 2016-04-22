857a4a6cce719376ffc4477202f861978fe0565f
valeronS <stankov.valeriy@mail.ru>
2016-04-22 18:00:42 +0300

    start the FileGenerator

---
+++ src/L19_IO_L20_Serialization/homeWork/FileGenerator.java
@@ -0,0 +1,42 @@
+package L19_IO_L20_Serialization.homeWork;
+
+import java.io.*;
+
+public class FileGenerator {
+
+	public static void writeFile(int size, byte[] part, String file) {
+		try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(file))) {
+			for (int i = 0; i < size; i++) {
+				bufferedOutputStream.write(part);
+			}
+			bufferedOutputStream.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Initializer _50KbyteInitializer = new Initializer() {
+			@Override
+			public int getFileSize() {
+				return 50;
+			}
+
+			@Override
+			public byte[] getPart() {
+				return Unit.K_BYTE.getPart();
+			}
+
+			@Override
+			public String getFilePath() {
+				return "FGenOutput/theFile.dat";
+			}
+		};
+
+		writeFile(_50KbyteInitializer.getFileSize(),
+				  _50KbyteInitializer.getPart(),
+				  _50KbyteInitializer.getFilePath()   );
+
+	}
+
+}
---
+++ src/L19_IO_L20_Serialization/homeWork/Initializer.java
@@ -0,0 +1,25 @@
+package L19_IO_L20_Serialization.homeWork;
+
+import java.util.Random;
+
+public interface Initializer {
+	enum Unit {
+		BYTE(1), K_BYTE(1024), M_BYTE(1024 * 1024);
+		private int size;
+		Random rand = new Random();
+
+		Unit(int size) {
+			this.size = size;
+		}
+		public byte[] getPart(){
+			byte[] part = new byte[size];
+			rand.nextBytes(part);
+			return part;
+		}
+	}
+
+	int getFileSize();
+	byte[] getPart();
+	String getFilePath();
+
+}
